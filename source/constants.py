0ebce5515e22b0c48f959c186d4c7df74a0a02cd
João Marafuz Gaspar <jfm.gaspar@gmail.com>
2022-12-16 22:44:32 +0000

    Converted pair (lat, lon) into (x, y) coordinates pair

--- source/constants.py
+++ source/constants.py
@@ -1,5 +1,5 @@
 import numpy as np
 
-ORIGIN = np.array([38.73762, -9.13896])  # Latitude/longitude of entrance of north tower
+ORIGIN = np.array([38.73762, -9.13896])  # Latitude and Longitude of entrance of north tower
 
 GMAPS_KEY = "AIzaSyAE3mAAR3DtUpmMY17pS18a7LeSzIbZXFI"
--- source/main.py
+++ source/main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 from blocks import Map
 from constants import ORIGIN
+from pyproj import Transformer
 
 
 def main():
@@ -12,12 +13,30 @@ def main():
 
     points = map.get_directions(ORIGIN, civil)
 
-    plt.plot(
-        points[:, 1],
-        points[:, 0],
-    )
+    # plt.plot(
+    #     points[:, 1],
+    #     points[:, 0],
+    # )
+    # plt.show()
+    
+    # Please try 3857 epsg code used by Google Maps
+    transform = Transformer.from_crs("epsg:4326", "+proj=utm +zone=10 +ellps=WGS84", always_xy=True,)
+    xx, yy = transform.transform(points[:, 0], points[:, 1])
+    
+    fig = plt.figure()
+    plt.grid()
+    plt.gca().set_aspect('equal', 'box')
+    plt.xlabel('Easting [m]', fontsize = 14)   
+    plt.ylabel('Northing [m]', fontsize = 14) 
+    plt.plot(xx, yy)
+    plt.gca().set_aspect('equal', 'box')
     plt.show()
-
-
+        
+    # a Pythagore's theorem is sufficient to compute an approximate distance
+    for i in range(len(xx) - 1):
+        distance = np.sqrt((xx[i + 1] - xx[i]) ** 2 + (yy[i + 1] - yy[i]) ** 2)
+        print(distance)
+    
+    
 if __name__ == "__main__":
     main()
