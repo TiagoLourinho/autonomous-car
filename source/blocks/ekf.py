96a88453a40d3794f5d2eb3753bead32c4ee0f5c
TiagoLourinho <tiagolourinho001@gmail.com>
2023-01-07 21:57:24 +0000

    made EKF easier to use (specific to the problem) and added matrices/functions

--- source/blocks/ekf.py
+++ source/blocks/ekf.py
@@ -3,77 +3,130 @@ from threading import Lock
 
 
 class EKF:
-    """Thread safe Extended Kalman Filter"""
+    """Thread safe Extended Kalman Filter
 
-    def init(
-        self,
-        initial_state: np.array,
-        initial_covariance: np.array,
-        G: np.array,
-        H: np.array,
-        Q: np.array,
-        R: np.array,
-        g: callable,
-        h: callable,
-    ):
+    To simplify the implementation the EKF parameters are static (specific to the implementation of the problem)
+    """
+
+    def init(self, car_L: float, predict_frequency: float):
         """
         For more info, see here: https://youtu.be/E-6paM_Iwfc?t=3622
         This one also helps (different connotation): https://www.kalmanfilter.net/multiSummary.html
-
-        Parameters
-        ----------
-        initial_state: np.array
-            Inital state estimate
-        initial_covariance: np.array
-            Initial covariance estimate
-        G: np.array
-            State Transition Matrix
-        H: np.array
-            Observation Matrix
-        Q: np.array
-            Process Noise Uncertainty
-        R: np.array
-            Measurement Uncertainty
-        g: callable
-            Expected inputs: previous state, present control
-            Expected output: present state
-        f: callable
-            Expected inputs: present state
-            Expected output: expected sensor readings
         """
 
-        self.state = initial_state
-        self.cov = initial_covariance
-        self.G = G
-        self.H = H
-        self.Q = Q
-        self.R = R
-        self.g = g
-        self.h = h
+        self.car_L = car_L
+        self.time_step = 1 / predict_frequency
+
+        self.state = np.zeros(shape=(8,))
+        self.cov = np.zeros(shape=(8, 8))
 
         self.lock = Lock()
 
     def predict(self, control: np.array) -> None:
         """Predict step"""
-        with self.lock:
-            self.state = self.g(control, self.state)
-            self.cov = self.G @ self.cov @ self.G.T + self.R
 
-    def update(self, measurements: np.array) -> None:
+        with self.lock:
+            A = np.array(
+                [
+                    [1, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 1, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 1, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 1, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                ]
+            )
+
+            B = np.array(
+                [
+                    [self.time_step * np.cos(self.state[2]) * np.cos(self.state[3]), 0],
+                    [self.time_step * np.sin(self.state[2]) * np.cos(self.state[3]), 0],
+                    [self.time_step * np.sin(self.state[3]) / self.car_L, 0],
+                    [0, self.time_step],
+                    [np.cos(self.state[2]) * np.cos(self.state[3]), 0],
+                    [np.sin(self.state[2]) * np.cos(self.state[3]), 0],
+                    [np.sin(self.state[3]) / self.car_L, 0],
+                    [0, 1],
+                ]
+            )
+
+            # fmt:off
+            G = np.array(
+                [
+                    [1, 0, -control[0]*self.time_step*np.sin(self.state[2])*np.cos(self.state[3]), -control[0]*self.time_step*np.cos(self.state[2])*np.sin(self.state[3]), 0, 0, 0, 0],
+                    [0, 1, control[0]*self.time_step*np.cos(self.state[2])*np.cos(self.state[3]), -control[0]*self.time_step*np.sin(self.state[2])*np.sin(self.state[3]), 0, 0, 0, 0],
+                    [0, 0, 1, control[0]*self.time_step*np.cos(self.state[3])/self.car_L, 0, 0, 0, 0],
+                    [0, 0, 0, 1, 0, 0, 0, 0],
+                    [1, 0, -control[0]*np.sin(self.state[2])*np.cos(self.state[3]), -control[0]*np.cos(self.state[2])*np.sin(self.state[3]), 0, 0, 0, 0],
+                    [0, 1, control[0]*np.cos(self.state[2])*np.cos(self.state[3]), -control[0]*np.sin(self.state[2])*np.sin(self.state[3]), 0, 0, 0, 0],
+                    [0, 0, 1, control[0]*np.cos(self.state[3])/self.car_L, 0, 0, 0, 0],
+                    [0, 0, 0, 1, 0, 0, 0, 0],
+                ]
+            )
+            # fmt:on
+
+            R = np.array(
+                [
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                ]
+            )
+
+            g = lambda controls, state: A @ state + B @ controls
+
+            self.state = g(control, self.state)
+            self.cov = G @ self.cov @ G.T + R
+
+    def update(self, measurements: np.array, sensor: str) -> None:
         """Update step"""
         with self.lock:
 
-            kalman_gain = (
-                self.cov
-                @ self.H.T
-                @ np.linalg.inv(self.H @ self.cov @ self.H.T + self.Q)
-            )
+            if sensor.lower() == "gps":
+                A = np.array(
+                    [
+                        [1, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 1, 0, 0, 0, 0, 0, 0],
+                    ]
+                )
 
-            self.state = self.state + kalman_gain * (measurements - self.h(self.state))
-            self.cov = (np.identity(like=self.H) - kalman_gain * self.H) @ self.cov
+                H = np.array(
+                    [
+                        [1, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 1, 0, 0, 0, 0, 0, 0],
+                    ]
+                )
 
-    def get_current_estimate(self) -> tuple[np.array]:
+                h = lambda state: A @ state
+
+                Q = np.array(
+                    [
+                        [0, 0],
+                        [0, 0],
+                    ]
+                )
+            elif sensor.lower() == "imu":
+                pass
+
+            kalman_gain = self.cov @ H.T @ np.linalg.inv(H @ self.cov @ H.T + Q)
+            self.state = self.state + kalman_gain * (measurements - h(self.state))
+            self.cov = (np.identity(like=H) - kalman_gain * H) @ self.cov
+
+    def get_current_estimate(self) -> dict:
         """Returns the current state estimate and estimate covariance"""
 
+        vars = ["x", "y", "theta", "phi", "dot_x", "dot_y", "dot_theta", "dot_phi"]
+
         with self.lock:
-            return self.state, self.cov
+
+            current = {var: self.state[i] for i, var in enumerate(vars)}
+            current["covariance"] = self.cov
+
+            return current
