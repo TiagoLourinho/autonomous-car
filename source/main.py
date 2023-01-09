cd8f53d0df940f0b8168178fd631874af660b7cf
TiagoLourinho <tiagolourinho001@gmail.com>
2023-01-09 00:04:08 +0000

    cleaned main

--- source/main.py
+++ source/main.py
@@ -7,56 +7,31 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 
-from matplotlib.patches import Circle
-from blocks import EKF, Car, Controller, Map, Sensors
+
+from blocks import EKF, Controller, Map, Sensors
 from constants import *
 
+OBJECTIVE = np.array([38.736911, -9.139010])  # Objetive position in lat/lon
 FREQUENCY = 100  # Hz
 
 # Thread related
 lock = Lock()
 thread_shutdown = False
 
-update_gui = False  # Signals if the car moved
-
+# Blocks
 map = Map()
-
-# Load the image file and convert it to a NumPy array
-image = plt.imread("images/map_improved.png")
-
-OBJECTIVE = np.array([38.736911, -9.139010])  # Objetive position in lat/lon
-
-# Get the map's corner coordinates
-top_right_corner = map.get_coordinates(*TOP_RIGHT_CORNER)
-bottom_left_corner = map.get_coordinates(BOTTOM_LEFT_CORNER[0], BOTTOM_LEFT_CORNER[1])
-
-
-# Get the coordinates of the origin point
+controller = Controller(qsi=1, w_n=10, v_ref=36, w_ref=4, h=0.01, L=2.2)
 origin = map.get_coordinates(ORIGIN[0], ORIGIN[1]).reshape((2,))
-
 ekf = EKF(origin, FREQUENCY)
-
-# Set the x-axis and y-axis limits to center the origin point
-x_lims = [
-    round(bottom_left_corner[0][0]) - round(origin[0]),
-    round(top_right_corner[0][0]) - round(origin[0]),
-]
-y_lims = [
-    round(bottom_left_corner[0][1]) - round(origin[1]),
-    round(top_right_corner[0][1]) - round(origin[1]),
-]
-
-controller = Controller(
-    qsi=1, w_n=10, v_ref=36, w_ref=4, h=0.01, L=2.2
-)  # Control model for the steering wheel
 
 
 def sensor_thread():
-    """Checks for sensor data and updates EKF"""
+    """Function to run in a thread, checking for new sensor data and updating EKF"""
 
     global thread_shutdown
 
     sensors = Sensors()
+
     # Because in simulation there's always data available, let's
     # define a limit to how frequently we can poll
     gps_poll_freq = 1
@@ -65,6 +40,7 @@ def sensor_thread():
     last_imu_poll = 0
 
     while not thread_shutdown:
+        print("sensor running")
         sensors.acquire()
 
         state = ekf.get_current_state()
@@ -81,15 +57,40 @@ def sensor_thread():
             last_imu_poll = time.time()
 
 
-def update_plot(n, state):
-    """Updates the plot"""
+def control_thread(path):
+    """Function to run in a thread, calculating the control signals"""
+
+    global thread_shutdown
+
+    for point in map.orient_path(path):
+        while True:
+            print("control running")
+            pose = ekf.get_current_state()[:3]
+            current_control = controller.following_trajectory(point, pose)
+            ekf.predict(current_control)
+
+            sleep(1 / FREQUENCY)
+
+            position = ekf.get_current_state()[:2]
+
+            # Move to next point if close enough to the current one
+            if np.linalg.norm(position - point[:2]) < 2:
+                break
+
+            if thread_shutdown:
+                return
+
+    # Terminate program when the objetive was reached
+    thread_shutdown = True
+
+
+def update_animation(n, state):
+    """Updates the plot (used by FuncAnimation)"""
 
     axes = state["artists"]["axes"]
 
-    # Drive the car and get the result state
     position = ekf.get_current_state()[:3]
 
-    # Update car position and orientation
     state["artists"]["car_position"].set_data(
         position[0] - round(origin[0]), position[1] - round(origin[1])
     )
@@ -111,33 +112,36 @@ def update_plot(n, state):
     ]
 
 
-def display_current_state(path):
-    """Displays the path and the current state"""
+def start_gui(path):
+    """Displays the path and the"""
+
     state = {"artists": dict()}
+    image = plt.imread("images/map_improved.png")
 
-    # Plot the point on the map
-    fig, ax = plt.subplots()  # Create a figure and axes object
+    fig, ax = plt.subplots()
 
-    # Set the x-axis and y-axis limits to center the origin point
+    top_right_corner = map.get_coordinates(*TOP_RIGHT_CORNER).reshape((2,))
+    bottom_left_corner = map.get_coordinates(*BOTTOM_LEFT_CORNER).reshape((2,))
+
     x_lims = [
-        round(bottom_left_corner[0][0]) - round(origin[0]),
-        round(top_right_corner[0][0]) - round(origin[0]),
+        round(bottom_left_corner[0]) - round(origin[0]),
+        round(top_right_corner[0]) - round(origin[0]),
     ]
     y_lims = [
-        round(bottom_left_corner[0][1]) - round(origin[1]),
-        round(top_right_corner[0][1]) - round(origin[1]),
+        round(bottom_left_corner[1]) - round(origin[1]),
+        round(top_right_corner[1]) - round(origin[1]),
     ]
 
     ax.set_xlim(x_lims[0], x_lims[1])
     ax.set_ylim(y_lims[0], y_lims[1])
-    ax.imshow(
-        image, extent=[x_lims[0], x_lims[1], y_lims[0], y_lims[1]]
-    )  # Plot the image on the axes
 
+    ax.imshow(image, extent=[x_lims[0], x_lims[1], y_lims[0], y_lims[1]])
+
+    # Plotting path
     for i in range(len(path)):
         x = path[i][0] - round(origin[0])
         y = path[i][1] - round(origin[1])
-        ax.plot(x, y, "ro", markersize=3)  # Plot the path on the axes
+        ax.plot(x, y, "ro", markersize=3)
 
     state["artists"]["axes"] = ax
     (state["artists"]["car_position"],) = ax.plot([], [], "bo", markersize=10)
@@ -145,46 +149,19 @@ def display_current_state(path):
 
     anim = animation.FuncAnimation(
         fig,
-        lambda n: update_plot(n, state),
+        lambda n: update_animation(n, state),
         frames=None,
-        interval=1000 / FREQUENCY,
+        interval=5000,
         blit=True,
     )
 
-    # Show the plot
     plt.show()
 
 
-def control_thread(path):
-    """Updates the current controls acordding the current state and desired path"""
-    global update_gui
-    global thread_shutdown
-    for point in map.orient_path(path):
-        while True:
-
-            pose = ekf.get_current_state()[:3]
-            current_control = controller.following_trajectory(point, pose)
-            ekf.predict(current_control)
-
-            with lock:
-                update_gui = True
-
-            sleep(1 / FREQUENCY)
-
-            position = ekf.get_current_state()[:2]
-
-            if np.linalg.norm(position - point[:2]) < 2:
-                break
-
-    thread_shutdown = True
-
-
 def main():
-    initial_position = ORIGIN
+    global thread_shutdown
 
-    path = map.get_path(
-        initial_position, OBJECTIVE
-    )  # returns coordinates with (0, 0) being the bottom left corner of the map
+    path = map.get_path(ORIGIN, OBJECTIVE)
 
     threads = {
         "sensor_thread": Thread(target=sensor_thread),
@@ -195,12 +172,11 @@ def main():
         t.start()
 
     try:
-        display_current_state(path)
+        start_gui(path)
     except Exception:
         print(traceback.format_exc())
-        thread_shutdown = True
-
     finally:
+        thread_shutdown = True
         for t in threads.values():
             t.join()
 
